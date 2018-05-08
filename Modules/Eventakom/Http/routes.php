b9a9c5543e32de03b1747702e1cc16ef6731962c
unknown <ashrafhesham-92>
2018-05-08 14:00:58 +0200

    module deteted

--- Modules/Eventakom/Http/routes.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-Route::group(['middleware' => 'web', 'prefix' => 'eventakom', 'namespace' => 'Modules\Eventakom\Http\Controllers'], function()
-{
-    Route::get('/', 'EventakomController@index');
-});
--- Modules/Eventakom/Resources/views/index.blade.php
+++
@@ -1,9 +0,0 @@
-@extends('eventakom::layouts.master')
-
-@section('content')
-    <h1>Hello World</h1>
-
-    <p>
-        This view is loaded from module: {!! config('eventakom.name') !!}
-    </p>
-@stop
