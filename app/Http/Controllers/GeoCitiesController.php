a00ca0876c6965b7fc1c03070eb7ab52dd5793c9
omar ibrahim <eng.omarmm@gmail.com>
2018-05-26 12:48:06 +0200

    localization in search cities solved retest#2

--- app/Http/Controllers/GeoCitiesController.php
+++ app/Http/Controllers/GeoCitiesController.php
@@ -12,13 +12,17 @@ class GeoCitiesController extends Controller
         return response()->json(GeoCity::all());
     }
 
-    public function getcitycountry()
+    public function getcitycountry(Request $request)
     {
+    $lang_id = $request->input('lang_id');
+    $locale =Helpers::Set_locale($lang_id);
+    //dd($lang_id);
       $cities= GeoCity::all();
       $citycounty= array();
       foreach($cities as $key=>$city){
 
-$citycounty[$key]= $city->name.','.$city->geo_country->name;
+// $citycounty[$key]= $city->name.','.$city->geo_country->name;
+$citycounty[$key]= $city->getNameAttribute($city->name).','.$city->geo_country->getNameAttribute($city->geo_country->name);
 
       }
         return response()->json($citycounty);
@@ -30,36 +34,41 @@ $citycounty[$key]= $city->name.','.$city->geo_country->name;
      $lang_id = $request->input('lang_id');
      $keyword = $request->input('keyword');
     //dd($keyword);
-     
+      $result= array();
+    $locale =Helpers::Set_locale($lang_id);
       //return;
 
       if ($keyword!='') {
            // $citycounty = GeoCity::where("name", "LIKE","%$keyword%")
            //        ->orWhere($citycounty->geo_country->name, "LIKE", "%$keyword%");
-
+   if($lang_id ==1){
     $citycounty = GeoCity::where('name','like','%'.$keyword.'%')
      ->orWhereHas('geo_country', function ($query) use ($keyword) {
          $query->where('name', 'like', '%'.$keyword.'%');
      })->get();
+     foreach($citycounty as $key=>$city){
+
+     $result[$key]= $city->getNameAttribute($city->name).','.$city->geo_country->getNameAttribute($city->geo_country->name);
+// dd($result);
+     }
+   }elseif($lang_id ==2){
+  //$citycounty = GeoCity::city_entity_ar()->where('entity_localizations.value','like','%'.$keyword.'%')->get(); city works
+  $citycounty = GeoCity::city_entity_ar()->where('entity_localizations.value','like','%'.$keyword.'%')
+   ->orWhereHas('geo_country', function ($query) use ($keyword) {
+     $query->join('entity_localizations','geo_countries.id','=','entity_localizations.item_id')
+     ->where('entity_id','=',8)->where('field','=','name')
+     ->select('geo_countries.*','entity_localizations.value')->where('entity_localizations.value','like','%'.$keyword.'%');
+   })->get();
+   foreach($citycounty as $key=>$city){
+
+   $result[$key]= $city->getNameAttribute($city->name).','.$city->geo_country->getNameAttribute($city->geo_country->name);
+
+   }
+
+   }
     // $citycounty = GeoCity::where('name','like','%'.urldecode($keyword).'%')->get();
-    $result= array();
-    foreach($citycounty as $key=>$city){
 
-    $result[$key]= $city->name.','.$city->geo_country->name;
-//             if( $lang_id == 1){
-//          $city->name =  $city->name;
-// $result[$key]= $city->name.','.$city->geo_country->name;
-//                   }elseif( $lang_id == 2){
-//                 $cityname =  Helpers::localization('geo_cities', 'name', $city->id, $lang_id );
-//                 if($cityname == "Error"){$city->name =  $city->name;
-//                 }else{
-//                     $city->name = $cityname;
-//                 }
-//                 $result[$key]= $city->name.','.$city->geo_country->name;
-//             }
- 
 
-    }
     if (!empty($result)) {
         return Helpers::Get_Response(200,'success','','',$result);
     }else{
