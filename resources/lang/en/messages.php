7cfb15a9a014e5d79da4d6b45e419d2182da6eff
ashrafhesham-92 <ashraf_treka_6000@hotmail.com>
2018-05-26 02:19:54 +0200

    Events updates

--- resources/lang/en/messages.php
+++ resources/lang/en/messages.php
@@ -26,7 +26,7 @@ return [
 	'noaccess'=>'you have no access to show this case',
 	'noservices'=>'no services yet',
 	'wrong_user_password' =>'wrong user password',
-	'interest_not_found' =>'The interest you entered not found'
+	'interest_not_found' =>'The interest you entered not found',
 	'wrong_mobile_verification_code'=>'wrong mobile verification code'
 
 
